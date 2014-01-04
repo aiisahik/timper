ba2acd7adf418838bc2f4870250b445715bf5908
aiisahik <jie.z.zhou@gmail.com>
2014-01-03 20:25:00 -0500

    added procfile"

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.set('port', process.env.PORT || 3000);
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', function(req, res){
   res.send('Hello World');
 });
